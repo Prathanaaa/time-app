9fc7fb2c73c78540ae4511f1c5792d2cc01aff5d
Anais <anais.gouault@gmail.com>
2023-02-23 20:56:29 +1100

    Added Scripts for main date and time

--- javascript/app.js
+++ javascript/app.js
@@ -1,1 +1,9 @@
+let bangkokElement = document.querySelector("#bangkok");
+let bangkokDateElement = bangkokElement.querySelector(".date");
+let bangkokTimeElement = bangkokElement.querySelector(".time");
+let bangkokTime = moment();
 
+bangkokDateElement.innerHTML = bangkokTime.format("dddd Do MMMM");
+bangkokTimeElement.innerHTML = bangkokTime.format(
+  "h:mm:ss"[(<small>A</small>)]
+);
